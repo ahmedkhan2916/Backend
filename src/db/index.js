5a6fb54bf880abb57221f65949e1d6f8389a760a
ahmed2916 <ahmedkhandelhi123@gmail.com>
2024-03-26 01:40:31 +0530

    connect with mongodb atlas

---
+++ src/db/index.js
@@ -0,0 +1,25 @@
+// require ('dotenv').config({path:'../../env'})
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { DBNAME } from "../constants.js";
+import express from "express";
+
+const app=express();
+
+const connectDb=async ()=>{
+    
+try{
+
+       const connectInstance= await mongoose.connect(`${process.env.MONGODB_URL}/${DBNAME}`);
+       console.log("dbConnected Successfuly");
+    }
+    catch(error)
+    {
+console.log(error)
+    }
+}
+
+export default connectDb;
+    
+
+
